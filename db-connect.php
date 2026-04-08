ff62fa0e3de3469312d2b269b1d6895a5414c0e9
hnimXgibhut <minhb2306675@student.ctu.edu.vn>
2026-04-08 16:13:23 +0700

    Function, Css

--- db-connect.php
+++ db-connect.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'playnrate');
+define('DB_NAME', 'game_rating');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
